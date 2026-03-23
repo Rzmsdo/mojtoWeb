c3d5d99243b7d66fc7c74d3e4baa4240060ebc1c
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2026-03-23 00:35:21 +0000

    Hero cong

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,25 @@ export default function Hero() {
     return (
         <>
         <section id="hero" className="noisy">
-            <h1 className="title">Welcome to Velvet Pour</h1>
-            <p className="text-lg text-center max-w-2xl">Discover the art of mixology with our exquisite cocktail recipes, crafted to tantalize your taste buds and elevate your drinking experience.</p>
-            <a href="#cocktails" className="px-6 py-3 bg-gradient-to-r from-purple-500 to-pink-500 text-white rounded-full hover:from-purple-600 hover:to-pink-600 transition duration-300">Explore Cocktails</a>
-        </section>
+            <h1 className="title">Mojito</h1>
+           
+           <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
+           <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
+
+        <div className="body">
+            <div className="content">
+             <div className="space-y-5 hidden md:block">
+            <p>Cool. Crips. classic.</p>
+            <p className="subtitle">
+                Sip the spirit <br/> of Smmer
+            </p>
+            
+            </div>
+            </div>
+        </div>
+
+
+            </section>
         </>
     )
 }
